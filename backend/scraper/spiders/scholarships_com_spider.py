e441e50ef41e5586867d8dc484e1d8df7aba378a
Adan-Bhatti <adansaj987@gmail.com>
2026-06-18 19:39:46 +0500

    Create Scholarships.com Spider

---
+++ backend/scraper/spiders/scholarships_com_spider.py
@@ -0,0 +1,21 @@
+from scraper.base_spider import BaseScholarshipSpider
+
+class ScholarshipsComSpider(BaseScholarshipSpider):
+    name = "scholarships_com"
+    allowed_domains = ["scholarships.com"]
+    start_urls = ["https://www.scholarships.com/financial-aid/college-scholarships/"]
+
+    def parse(self, response):
+        # NOTE: Dry-run implementation
+        raw_data = {
+            "title": "Excellence in STEM Scholarship",
+            "provider": "Scholarships.com Foundation",
+            "description": "Targeted at students entering STEM fields.",
+            "amount_text": "$1000 - $3000",
+            "deadline_text": "March 15, 2026",
+            "source_url": response.url,
+            "fields_of_study": ["Computer Science", "Engineering"],
+            "degree_levels": ["Undergraduate", "High School"]
+        }
+        
+        yield self.normalize_item(raw_data)
